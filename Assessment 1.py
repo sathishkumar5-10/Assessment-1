1c981b387c8286b65bfaa264b838e0c2fb9fb418
sathishkumar5-10 <125422601+sathishkumar5-10@users.noreply.github.com>
2023-02-15 08:54:12 +0530

    Add files via upload

---
+++ Assessment 1.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x= "Hi Sathishkumar"
+y= "Hi How are you"
+X= input("Please introduce your self")
+print(x)
+
+
+# # Addition
+
+# In[ ]:
+
+
+A= int (input("Enter A value"))
+B =int (input("Enter B value"))
+print("The Addition of A and B is",A+B)
+
+
+# Sub
+
+# In[ ]:
+
+
+A= int (input("Enter A value"))
+B =int (input("Enter B value"))
+print("The Subtraction of A and B is",A-B)
+
+
+# Multi
+
+# In[5]:
+
+
+A= int (input("Enter A value"))
+B =int (input("Enter B value"))
+print("The Multiplication of A and B is",A*B)
+
+
+# Division
+
+# In[6]:
+
+
+A= int (input("Enter A value"))
+B =int (input("Enter B value"))
+print("The Division of A and B is",A/B)
+
+
+# Modulus
+
+# In[10]:
+
+
+A= int (input("Enter A value"))
+B =int (input("Enter B value"))
+print("The Modulus of A and B is",A%B)
+
+
+# Exponent
+
+# In[11]:
+
+
+A= int (input("Enter A value"))
+B =int (input("Enter B value"))
+print("The Exponent of A and B is",A**B)
+
+
+# Area of rectangle
+
+# In[ ]:
+
+
+L= int (input("Enter L value"))
+B =int (input("Enter B value"))
+print("Area of Rectangle",L*B)
+
+
+# Area of square
+
+# In[13]:
+
+
+Area= int (input("Enter A1 value"))
+print("Area of square",Area*Area)
+
+
+# Area of Triangle
+
+# In[14]:
+
+
+b=int(input("Enter the b value"))
+h=int(input("Enter the h value"))
+print("Area of triangle is",(1/2*b*h))
+
+
+# Area of circle
+
+# In[15]:
+
+
+Area=int(input("Enter the Area value"))
+print("Area of circle is", (3.14*Area*Area))
+
+
+# Net salary calculation
+
+# In[3]:
+
+
+BS=int(input("Enter the basic salary"))
+DA=(5/100*BS)
+HRA=(7/100*BS)
+PF=(3/100*BS)
+print("Net salary of an employee",BS+HRA+DA+PF)
+
+
+# In[4]:
+
+
+bv=int(input("Enter the base value"))
+Pow=int(input("Enter the power value"))
+print("The power of the number is",bv**Pow)
+
+
+# In[8]:
+
+
+import sys
+print("Version of python I am using is ",sys.version)
+
+
+# Swap of two number
+
+# In[12]:
+
+
+a=1
+b=2
+temp=a
+a=b
+b=temp
+print(a,b)
+
+
+# Swap of two number with out third number
+
+# In[13]:
+
+
+a=1
+b=2
+a,b=b,a
+print(a,b)
+
+
+# In[ ]:
+
+
+
+
